c45cdb49e3c6554a3edbc97adf071cf386084a18
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-12-13 00:20:20 +0530

    added the production route to the fetch api

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,8 +22,8 @@ export default function Form({setResult, resetForm, setResetForm}) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5001/predict", {
         // const response = await fetch("http://localhost:5001/predict", {
+        const response = await fetch("https://stroke-prediction-server-production.up.railway.app/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
